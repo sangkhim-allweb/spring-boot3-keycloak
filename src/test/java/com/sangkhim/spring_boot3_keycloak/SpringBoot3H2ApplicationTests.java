5bf4b11cf718ee8d3ac746005ca97f98c3ee5fb7
sangkhim <sangkhimpay@gmail.com>
2023-07-23 21:15:54 +0700

    format code

--- src/test/java/com/sangkhim/spring_boot3_keycloak/SpringBoot3H2ApplicationTests.java
+++ src/test/java/com/sangkhim/spring_boot3_keycloak/SpringBoot3H2ApplicationTests.java
@@ -1,10 +1,26 @@
 package com.sangkhim.spring_boot3_keycloak;
 
+import java.util.Iterator;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot3H2ApplicationTests {
+
+  public SpringBoot3H2ApplicationTests() {
+    // list all the caching provider
+    Iterator<CachingProvider> iterator =
+        Caching.getCachingProviders(Caching.getDefaultClassLoader()).iterator();
+    while (iterator.hasNext()) {
+      CachingProvider provider = iterator.next();
+      if (!(provider instanceof EhcacheCachingProvider)) {
+        iterator.remove();
+      }
+    }
+  }
 
   @Test
   void contextLoads() {}
