018273b892201f8a59718a6e18921962bfb29615
sangkhim <sangkhim.allweb@gmail.com>
2023-07-25 16:24:55 +0700

    allow h2 console

--- src/main/java/com/sangkhim/spring_boot3_keycloak/config/WebSecurityConfig.java
+++ src/main/java/com/sangkhim/spring_boot3_keycloak/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sangkhim.spring_boot3_keycloak.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,9 +23,17 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
+        .requestMatchers(toH2Console())
         .permitAll()
-        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+        .and()
+        .csrf()
+        .ignoringRequestMatchers(toH2Console())
+        .and()
+        .headers()
+        .frameOptions()
+        .sameOrigin();
+    http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
         .permitAll()
         .requestMatchers(HttpMethod.GET, "/v1/authors/**", "/v1/tags/**")
         .hasRole(ADMIN)
