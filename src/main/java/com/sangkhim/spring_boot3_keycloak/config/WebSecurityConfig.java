6fae1b95a8b6bc334ef0c8c3857b5372f70281ce
sangkhim <sangkhim.allweb@gmail.com>
2023-07-26 17:07:44 +0700

    update spring security from 3.0 to 3.1

--- src/main/java/com/sangkhim/spring_boot3_keycloak/config/WebSecurityConfig.java
+++ src/main/java/com/sangkhim/spring_boot3_keycloak/config/WebSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.sangkhim.spring_boot3_keycloak.config;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -22,33 +23,43 @@ public class WebSecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.securityMatcher("/**")
+        .authorizeHttpRequests(
+            rmr ->
+                rmr.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
+                    .permitAll());
+
     // h2-console
-    http.authorizeHttpRequests()
-        .requestMatchers(toH2Console())
-        .permitAll()
-        .and()
-        .csrf()
-        .ignoringRequestMatchers(toH2Console())
-        .and()
-        .headers()
-        .frameOptions()
-        .sameOrigin();
+    http.securityMatcher(toH2Console())
+        .authorizeHttpRequests(rmr -> rmr.requestMatchers(toH2Console()).permitAll())
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(
+            httpSecurityHeadersConfigurer ->
+                httpSecurityHeadersConfigurer.frameOptions(
+                    HeadersConfigurer.FrameOptionsConfig::disable))
+        .headers(
+            httpSecurityHeadersConfigurer ->
+                httpSecurityHeadersConfigurer.frameOptions(
+                    HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
-    http.authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
-        .permitAll()
-        .requestMatchers(HttpMethod.GET, "/v1/authors/**", "/v1/tags/**")
-        .hasRole(ADMIN)
-        .requestMatchers(HttpMethod.GET, "/v1/**")
-        .hasAnyRole(ADMIN, USER)
-        .anyRequest()
-        .authenticated();
-    http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.securityMatcher("/v1/**")
+        .authorizeHttpRequests(
+            rmr ->
+                rmr.requestMatchers("/v1/authors/**", "/v1/tags/**")
+                    .hasRole(ADMIN)
+                    .requestMatchers("/v1/authors/**", "/v1/tags/**")
+                    .hasAnyRole(ADMIN, USER)
+                    .requestMatchers("/v1/**")
+                    .authenticated());
 
-    // Exception handling
-    http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
-    http.exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler());
+    http.oauth2ResourceServer(
+        oauth2ResourceServer ->
+            oauth2ResourceServer
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+
+    http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS));
 
     return http.build();
   }
